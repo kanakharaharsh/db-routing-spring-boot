bbf169cc34e2200a3bd95a9873d25dcb271786b0
kanakharaharsh <harshkanakhara@gmail.com>
2024-04-15 15:44:20 +0530

    Removing experimental code

--- src/main/java/com/db/routing/example/dbroutingexample/service/DatabaseTestingService.java
+++ src/main/java/com/db/routing/example/dbroutingexample/service/DatabaseTestingService.java
@@ -3,7 +3,6 @@ package com.db.routing.example.dbroutingexample.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 // Service class annotated with @Transactional(readOnly = true). It means if @Transactional is not present, request will by default go to replica DB
 @Transactional(readOnly = true)
 public class DatabaseTestingService {
-	
-	private final DatabaseTestingService self;
-	
-	@Lazy
-	public DatabaseTestingService(DatabaseTestingService self) {
-		this.self = self;
-	}
 	
 	@Autowired
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
